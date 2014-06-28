ce1444e3009f18206b9d7ce6c64b4e95f1eabd5d
Sharif Olorin <sio@tesser.org>
2014-06-28 20:27:11 +1000

    Kinda-working skeleton
    Signed-off-by: Sharif Olorin <sio@tesser.org>

---
+++ Main.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module Main where
+
+import Pdf.Toolbox.Document
+import Options.Applicative
+import System.IO
+import Data.Text (unpack)
+
+data ExtractorOptions = ExtractorOptions {
+    argFilename :: String
+}
+
+opts :: Parser ExtractorOptions
+opts = ExtractorOptions <$> argument str (metavar "FILENAME")
+
+extractorOptionParser :: ParserInfo ExtractorOptions
+extractorOptionParser =
+    info (helper <*> opts)
+    (fullDesc <> 
+        progDesc "Outputs the text content of a PDF file" <>
+        header "pdf2txt - extracts the textual content of a PDF file")
+
+extractFile :: ExtractorOptions -> IO ()
+extractFile ExtractorOptions{..} = do
+    res <- withBinaryFile argFilename ReadMode $ \handle ->
+        runPdfWithHandle handle knownFilters $ do
+            pgRoot <- document >>= documentCatalog >>= catalogPageNode 
+            nPages <- pageNodeNKids pgRoot
+            pages <- mapM (pageNodePageByNum pgRoot) (enumFromTo 0 (nPages - 1))
+            txts <- mapM pageExtractText pages
+            liftIO $ mapM_ (putStrLn . unpack) txts
+    case res of
+        Left badness -> hPutStr stderr $ "PDF error: " ++ show badness
+        Right _ -> return ()
+
+main :: IO ()
+main = execParser extractorOptionParser >>= extractFile
